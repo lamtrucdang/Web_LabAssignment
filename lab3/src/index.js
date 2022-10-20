ae1c8cdd8fb86350eef78eb8ef8bed53abae2a4e
Lam Truc Dang <19521736@gm.uit.edu.vn>
2022-10-20 17:32:34 +0700

    update 1.3

--- lab3/src/index.js
+++ lab3/src/index.js
@@ -12,41 +12,60 @@ import MyComponent from './Ex1/Ex1';
 import MyButton1 from './Ex1/Ex1-2';
 import MyList from './Ex1/MyList';
 
+import MyButton2 from './Ex1/Ex1-3';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const appState = {
-  text: "My Button",
-  disabled: true,
-  items: ["First", "Second", "Third"],
-};
+// const appState = {
+//   text: "My Button",
+//   disabled: true,
+//   items: ["First", "Second", "Third"],
+// };
 
-function render1(props){
-  root.render(
-    <main>
-      <MyButton1 text={props.text} disabled={props.disabled} />
-      <MyList items={props.items} />
-    </main>
-  );
-}
+// function render1(props){
+//   root.render(
+//     <main>
+//       <MyButton1 text={props.text} disabled={props.disabled} />
+//       <MyList items={props.items} />
+//     </main>
+//   );
+// }
 
-render(appState);
+// render(appState);
 
-setTimeout(() => {
-  appState.disabled = false;
-  appState.items.push("Fourth");
+// setTimeout(() => {
+//   appState.disabled = false;
+//   appState.items.push("Fourth");
   
-  render(appState);
-}, 1000);
+//   render(appState);
+// }, 1000);
 
-// root.render( 
-//   // <React.StrictMode>
-//   //   <App />
-//   // </React.StrictMode>
-//   // <MySection>
-//   //   <MyButton>My Button Text</MyButton>
-//   // </MySection>
-//   <MyComponent/>
-// );
+// function render2({ second }) {
+//   root.render(
+//     <main>
+//       <MyButton2 />
+//       <MyButton2 text={second.text} disabled={second.disabled} />
+//     </main>
+//   );
+// }
+
+// render({
+//   second: {
+//     text: "Second Button",
+//     disabled: true,
+//   },
+// });
+
+root.render( 
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+  // <MySection>
+  //   <MyButton>My Button Text</MyButton>
+  // </MySection>
+  // <MyComponent/>
+);
 
 reportWebVitals();
