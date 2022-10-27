98c64466e7af0dfde6fa6bb522cc619527b27751
Lam Truc Dang <19521736@gm.uit.edu.vn>
2022-10-27 16:40:02 +0700

    update lab3

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,17 +1,65 @@
-import './App.css';
-import Ex0 from './Ex0';
-import Ex01 from './Ex01';
-import Ex02 from './Ex02';
-import Ex04 from './Ex04';
+// import './App.css';
+// import Ex0 from './Ex0';
+// import Ex01 from './Ex01';
+// import Ex02 from './Ex02';
+// import Ex04 from './Ex04';
+
+// function App() {
+//   return (
+//     <div>
+//           <Ex0/>
+//           <Ex01/>
+//           <Ex02/>
+//           <Ex04/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//--------------------------------------------------------------
+//For exercise 2.1
+
+// import * as React from 'react';
+
+// export default function App() {
+//   const [name] = React.useState('Adam');
+//   const [age] = React.useState(35);
+
+//   return (
+//     <>
+//       <p>My name is {name}</p>
+//       <p>My age is {age}</p>
+//     </>
+//   );
+// }
+//--------------------------------------------------------------
+
+//For exercise 2.2, 2.3
+import * as React from 'react';
 
 function App() {
+  const [name, setName] = React.useState('Adam');
+  const [age, setAge] = React.useState(35);
+
   return (
-    <div>
-          <Ex0/>
-          <Ex01/>
-          <Ex02/>
-          <Ex04/>
-    </div>
+    <>
+      <section>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <p>My name is {name}</p>
+      </section>
+      <section>
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <p>My age is {age}</p>
+      </section>
+    </>
   );
 }
 
